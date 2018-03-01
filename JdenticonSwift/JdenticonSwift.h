3f399aee22f494fc81042d90a655c3d15748fc40
Alejandro Isaza <al@isaza.ca>
2018-02-28 21:08:10 -0800

    Initial Commit

---
+++ JdenticonSwift/JdenticonSwift.h
@@ -0,0 +1,19 @@
+//
+//  JdenticonSwift.h
+//  JdenticonSwift
+//
+//  Created by Alejandro Isaza on 2018-02-28.
+//  Copyright © 2018 Alejandro Isaza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JdenticonSwift.
+FOUNDATION_EXPORT double JdenticonSwiftVersionNumber;
+
+//! Project version string for JdenticonSwift.
+FOUNDATION_EXPORT const unsigned char JdenticonSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JdenticonSwift/PublicHeader.h>
+
+
